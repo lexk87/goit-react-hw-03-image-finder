60b6175a89fe10055d0b9064d5e9fe1b378d5baf
Alexandr Katyshev <katyshev1@gmail.com>
2023-03-09 22:13:12 +0200

    added main components and api service

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+    render() {
+        return (
+            <>
+                <Searchbar />
+                <ImageGallery />
+                <Loader />
+                <Button />
+            </>
+        );
+    }
+}
