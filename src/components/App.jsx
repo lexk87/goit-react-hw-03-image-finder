f1294b6713995e9bfdd95ab6cd024c8f17b3198c
Alexandr Katyshev <katyshev1@gmail.com>
2023-03-13 12:54:47 +0200

    fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export class App extends Component {
     state = {
         images: [],
-        searchQuery: null,
+        searchQuery: '',
         pageNumber: 1,
         totalPages: null,
         isLoading: false,
@@ -17,20 +17,49 @@ export class App extends Component {
     async componentDidUpdate(_, prevState) {
         const { searchQuery, pageNumber } = this.state;
 
-        if (prevState.pageNumber !== pageNumber && pageNumber !== 1) {
+        if (searchQuery === '') {
+            return;
+        }
+
+        if (
+            prevState.pageNumber !== pageNumber ||
+            prevState.searchQuery !== searchQuery
+        ) {
             this.setState({ isLoading: true });
             const result = await getImages(searchQuery, pageNumber);
+
+            if (result.hits.length === 0) {
+                this.setState({ isLoading: false, isNotEmpty: false });
+                toast.error(
+                    'Sorry, there are no images matching your search request.',
+                    {
+                        position: 'top-right',
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: 'colored',
+                    }
+                );
+                return;
+            }
+
+            const totalPages = Math.ceil(result.totalHits / 12);
+
             this.setState(({ images }) => ({
                 images: [...images, ...result.hits],
+                totalPages: totalPages,
                 isLoading: false,
+                isNotEmpty: true,
             }));
         }
     }
 
-    onSubmit = async e => {
+    onSubmit = e => {
         e.preventDefault();
         const inputValue = e.target.elements.searchField.value.trim();
-        const pageNumber = 1;
 
         if (inputValue === '') {
             this.setState({ isNotEmpty: false });
@@ -44,40 +73,9 @@ export class App extends Component {
                 progress: undefined,
                 theme: 'colored',
             });
-            return;
         }
 
-        this.setState({ isLoading: true });
-        const result = await getImages(inputValue, pageNumber);
-        this.setState({ isLoading: false });
-
-        if (result.hits.length === 0) {
-            this.setState({ isNotEmpty: false });
-            toast.error(
-                'Sorry, there are no images matching your search request.',
-                {
-                    position: 'top-right',
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: 'colored',
-                }
-            );
-            return;
-        }
-
-        const totalPages = Math.floor(result.totalHits / 12);
-
-        this.setState({
-            images: result.hits,
-            searchQuery: inputValue,
-            pageNumber: pageNumber,
-            totalPages: totalPages,
-            isNotEmpty: true,
-        });
+        this.setState({ images: [], searchQuery: inputValue, pageNumber: 1 });
     };
 
     loadMore = () => {
@@ -119,3 +117,119 @@ export class App extends Component {
         );
     }
 }
+
+// export class App extends Component {
+//     state = {
+//         images: [],
+//         searchQuery: null,
+//         pageNumber: 1,
+//         totalPages: null,
+//         isLoading: false,
+//         isNotEmpty: false,
+//     };
+
+//     async componentDidUpdate(_, prevState) {
+//         const { searchQuery, pageNumber } = this.state;
+
+//         if (prevState.pageNumber !== pageNumber && pageNumber !== 1) {
+//             this.setState({ isLoading: true });
+//             const result = await getImages(searchQuery, pageNumber);
+//             this.setState(({ images }) => ({
+//                 images: [...images, ...result.hits],
+//                 isLoading: false,
+//             }));
+//         }
+//     }
+
+//     onSubmit = async e => {
+//         e.preventDefault();
+//         const inputValue = e.target.elements.searchField.value.trim();
+//         const pageNumber = 1;
+
+//         if (inputValue === '') {
+//             this.setState({ isNotEmpty: false });
+//             toast.warn('Your search request is empty!', {
+//                 position: 'top-right',
+//                 autoClose: 3000,
+//                 hideProgressBar: false,
+//                 closeOnClick: true,
+//                 pauseOnHover: true,
+//                 draggable: true,
+//                 progress: undefined,
+//                 theme: 'colored',
+//             });
+//             return;
+//         }
+
+//         this.setState({ isLoading: true });
+//         const result = await getImages(inputValue, pageNumber);
+//         this.setState({ isLoading: false });
+
+//         if (result.hits.length === 0) {
+//             this.setState({ isNotEmpty: false });
+//             toast.error(
+//                 'Sorry, there are no images matching your search request.',
+//                 {
+//                     position: 'top-right',
+//                     autoClose: 3000,
+//                     hideProgressBar: false,
+//                     closeOnClick: true,
+//                     pauseOnHover: true,
+//                     draggable: true,
+//                     progress: undefined,
+//                     theme: 'colored',
+//                 }
+//             );
+//             return;
+//         }
+
+//         const totalPages = Math.floor(result.totalHits / 12);
+
+//         this.setState({
+//             images: result.hits,
+//             searchQuery: inputValue,
+//             pageNumber: pageNumber,
+//             totalPages: totalPages,
+//             isNotEmpty: true,
+//         });
+//     };
+
+//     loadMore = () => {
+//         this.setState(({ pageNumber }) => ({
+//             pageNumber: pageNumber + 1,
+//         }));
+//     };
+
+//     render() {
+//         const { images, pageNumber, totalPages, isLoading, isNotEmpty } =
+//             this.state;
+//         const isNotListEnd = pageNumber < totalPages;
+
+//         return (
+//             <>
+//                 <Searchbar onSubmit={this.onSubmit} />
+//                 {isNotEmpty && <ImageGallery images={images} />}
+//                 {isLoading ? (
+//                     <Loader />
+//                 ) : (
+//                     isNotEmpty &&
+//                     isNotListEnd && <Button onClick={this.loadMore} />
+//                 )}
+//                 {!isNotListEnd && isNotEmpty && <ListEnd />}
+
+//                 <ToastContainer
+//                     position="top-right"
+//                     autoClose={3000}
+//                     hideProgressBar={false}
+//                     newestOnTop={false}
+//                     closeOnClick
+//                     rtl={false}
+//                     pauseOnFocusLoss
+//                     draggable
+//                     pauseOnHover
+//                     theme="colored"
+//                 />
+//             </>
+//         );
+//     }
+// }
